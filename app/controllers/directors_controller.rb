0ff022e4ff3e60b3651e5f77c98b776603e09328
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-09-06 19:00:50 +0000

    adds directors#show

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,7 +4,9 @@ class DirectorsController < ApplicationController
   end  
   def show
     the_id = params.fetch("the_id")
-    @x = Director.where ({ :id => "the_id"})
+
+    matching_records = Director.where({ :id => the_id})
+    @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
   end
 end
