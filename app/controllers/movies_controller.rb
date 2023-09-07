455e1a1e2a644673cfd0283f973c6c499586191a
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-09-07 14:48:16 +0000

    fixed movie controller update list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    render({ :template => "movies_templates/list" })
+    render({ :template => "movie_templates/list" })
   end  
   def show
     the_id = params.fetch("the_id")
 
-    matching_records = Movies.where({ :id => the_id})
+    matching_records = Movie.where({ :id => the_id})
     @the_movie = matching_records.at(0)
-    render({ :template => "director_templates/details"})
+    render({ :template => "movie_templates/details"})
   end
 end
