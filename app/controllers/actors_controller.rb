bd904b2fc61a91405ecf63742f710c85275bb4c0
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-09-07 19:14:37 +0000

    update actor controller + added actor#index actor#show

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,6 @@
 class ActorsController < ApplicationController
   def index
-    render({ :template => "actor_templates/list" })
+    render({ :template => "actor/list" })
   end  
   def show
     the_id = params.fetch("the_id")
@@ -9,6 +9,6 @@ class ActorsController < ApplicationController
 
     @the_actor = matching_records.at(0)
     
-    render({ :template => "actor_templates/details"})
+    render({ :template => "actor/details"})
   end
 end
